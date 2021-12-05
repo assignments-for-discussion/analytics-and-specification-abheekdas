d9bf28198d891f6cd3e379332d5417cc3b28b380
Abheek Das <33574432+abheekdas@users.noreply.github.com>
2021-12-06 00:08:02 +0530

    Update average.js

--- average.js
+++ average.js
@@ -1,5 +1,14 @@
 
 function average(numbers) {
+  
+  for (let i = 0; i<numbers.length; i++)
+  {
+    if(isNan(numbers[i]))
+    {
+      numbers[i] = 0;
+    }
+  }
+    
   return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
 }
 
