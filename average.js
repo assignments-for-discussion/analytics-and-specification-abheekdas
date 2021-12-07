88d975fed802c894877453b78de19d83df031b8e
Abheek Das <33574432+abheekdas@users.noreply.github.com>
2021-12-08 00:15:33 +0530

    Update average.js
    Implemented forEach Approach, and reduced usage of extra indices such as i

--- average.js
+++ average.js
@@ -1,23 +1,11 @@
 
 function average(numbers) {
-  
-  let result = 0;
-  let countNan = 0;
-  
-  for (let i = 0; i<numbers.length; i++)
-  {
-      
-    if(isNaN(numbers[i]))
-    {
-      countNan = countNan + 1;
-    }
-    else
-    {
-      result = result + numbers[i];
-    }
-  }
-  let newLength = numbers.length - countNan; 
-  return result/newLength;
+    
+    let result = 0;
+    let countNan = 0;
+    
+    numbers.forEach((number) => isNaN(number) ? countNan+=1 : result+=number);
+    return result / (numbers.length - countNan)
 }
 
 module.exports = {average};
