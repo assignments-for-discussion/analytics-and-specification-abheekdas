f949128a6463bdbc8c2cef93f7c170a8c2861df7
Abheek Das <33574432+abheekdas@users.noreply.github.com>
2021-12-08 13:15:48 +0530

    Update average.js

--- average.js
+++ average.js
@@ -1,14 +1,12 @@
-
 function average(numbers) {
-    
     let result = 0;
     let countNan = 0;
     
     numbers.forEach((number) => isNaN(number) ? countNan+=1 : result+=number);
+    
     if(countNan >= Math.floor(0.25*numbers.length)) // If 25% or more than 25% data is missing then we indicate sensor is malfunctioned.
         return NaN;
     else
         return result / (numbers.length - countNan)
 }
-
 module.exports = {average};
